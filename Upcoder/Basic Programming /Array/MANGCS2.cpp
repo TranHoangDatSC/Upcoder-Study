0e7e11efb263727ceb878c9bf4e116af40c56465
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2025-03-22 00:26:33 +0700

    Create MANGCS2.cpp

---
+++ Upcoder/Basic Programming /Array/MANGCS2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a[100];
+    int n = 0;
+    int x;
+  
+    while(cin >> x) {
+        a[n++] = x;
+    }
+  
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+}
