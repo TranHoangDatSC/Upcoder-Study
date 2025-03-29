c1c1b43f29c1ac6d6f5f12d4eadc7c31dbd525ef
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2025-03-29 14:13:36 +0700

    Create ASCII.cpp

---
+++ Upcoder/Basic Programming/Import - Export/ASCII.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int ASCII(char c) {
+    int result = 0;
+    return result = c;
+}
+
+int main() {
+    char c;
+    cin >> c;
+    
+    cout << ASCII(c);
+    return 0;
+}
