ee917f09ea1d3572ccfba1129496045ad8cc69b0
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2025-04-12 13:35:04 +0700

    Create LUYTHUA1.cpp

---
+++ Upcoder/Analysis and Algorithms/Recursive/LUYTHUA1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int Power(int a, int n) {
+    if(n == 0) return 1;
+    return a * Power(a, n - 1);
+}
+
+int main() {
+    int a,n;
+    cin >> a >> n;
+    cout << Power(a,n);
+    return 0;
+}
