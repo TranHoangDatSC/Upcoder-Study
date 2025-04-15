8dad8cfcd427a2e79c0fe08d490af2248d12dd40
TranHoangDatSC <148848718+TranHoangDatSC@users.noreply.github.com>
2025-04-15 23:09:02 +0700

    Create DEQUY7.cpp

---
+++ Upcoder/Analysis and Algorithms/Recursive/DEQUY7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int Fibonacci(int n) {
+    if(n <= 2) return 1;
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for(int i = 1; i <= n; i++)
+        cout << Fibonacci(i) << " ";
+
+    return 0;
+}
